b753e94f94b527913f10967edcd704ab685f947c
TazviGumbo <90114576+TazviGumbo@users.noreply.github.com>
2025-03-21 14:47:44 +0200

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Anime/', // Replace with your repository name
+  base: '/AniVerse/', // Replace with your repository name
   server: {
     port: 5173, // Default port for Vite
     open: true, // Automatically open the browser
